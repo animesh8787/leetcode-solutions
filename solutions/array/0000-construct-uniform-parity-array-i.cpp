013770280ddea99c28ace99fae0d05a44d225270
Animesh <143503771+animesh8787@users.noreply.github.com>
2026-09-02 11:00:17 +0530

    Solve: Construct Uniform Parity Array I (Easy)

---
+++ solutions/array/0000-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,10 @@
+// Construct Uniform Parity Array I
+// Difficulty: Easy   Solved: 2026-09-02
+// https://leetcode.com/problems/construct-uniform-parity-array-i/
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
