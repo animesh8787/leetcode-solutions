4714c15a6fc3bf951c027fab6bb29c613a3b7a8d
Animesh <143503771+animesh8787@users.noreply.github.com>
2026-09-03 14:56:00 +0530

    Solve: Minimum Path Sum (Medium)

---
+++ solutions/array/0000-minimum-path-sum.cpp
@@ -0,0 +1,27 @@
+// Minimum Path Sum
+// Difficulty: Medium   Solved: 2026-09-03
+// https://leetcode.com/problems/minimum-path-sum/
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int> dp(n, 0);
+
+        dp[0] = grid[0][0];
+
+        for (int j = 1; j < n; j++)
+            dp[j] = dp[j - 1] + grid[0][j];
+
+        for (int i = 1; i < m; i++) {
+            dp[0] += grid[i][0];
+
+            for (int j = 1; j < n; j++)
+                dp[j] = grid[i][j] + min(dp[j], dp[j - 1]);
+        }
+
+        return dp[n - 1];
+    }
+};
