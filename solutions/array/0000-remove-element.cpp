74211102977c4367de5b8b0a0f1dc2068bbd2dff
Animesh <143503771+animesh8787@users.noreply.github.com>
2026-09-03 14:55:36 +0530

    Solve: Remove Element (Easy)

---
+++ solutions/array/0000-remove-element.cpp
@@ -0,0 +1,19 @@
+// Remove Element
+// Difficulty: Easy   Solved: 2026-09-03
+// https://leetcode.com/problems/remove-element/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+};
