1844176747eaef9764daa25d5fd5aa06cc9cf873
Animesh <143503771+animesh8787@users.noreply.github.com>
2026-09-06 12:21:38 +0530

    Solve: Delete Operation for Two Strings (Medium)

---
+++ solutions/string/0000-delete-operation-for-two-strings.cpp
@@ -0,0 +1,32 @@
+// Delete Operation for Two Strings
+// Difficulty: Medium   Solved: 2026-09-06
+// https://leetcode.com/problems/delete-operation-for-two-strings/
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+
+        vector<int> dp(n + 1);
+
+        for (int j = 0; j <= n; j++)
+            dp[j] = j;
+
+        for (int i = 1; i <= m; i++) {
+            vector<int> next(n + 1);
+            next[0] = i;
+
+            for (int j = 1; j <= n; j++) {
+                if (word1[i - 1] == word2[j - 1])
+                    next[j] = dp[j - 1];
+                else
+                    next[j] = 1 + min(dp[j], next[j - 1]);
+            }
+
+            dp = next;
+        }
+
+        return dp[n];
+    }
+};
