b8d28a7ae5096f87794591c5f6e5f86d053074b1
Animesh <143503771+animesh8787@users.noreply.github.com>
2026-09-10 14:24:06 +0530

    Solve: Count and Say (Medium)

---
+++ solutions/string/0000-count-and-say.cpp
@@ -0,0 +1,30 @@
+// Count and Say
+// Difficulty: Medium   Solved: 2026-09-10
+// https://leetcode.com/problems/count-and-say/
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        string s = "1";
+
+        for (int i = 2; i <= n; i++) {
+            string next;
+
+            for (int j = 0; j < s.size();) {
+                int k = j;
+
+                while (k < s.size() && s[k] == s[j])
+                    k++;
+
+                next += to_string(k - j);
+                next += s[j];
+
+                j = k;
+            }
+
+            s = next;
+        }
+
+        return s;
+    }
+};
